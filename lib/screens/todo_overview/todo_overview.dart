04dcc8fd0e00232ed09c3fcb566bd6f1cba6d89e
Geraldo Hendriks <geraldohendriks@gmail.com>
2024-04-08 20:01:58 +0200

    Created TodoOverview

---
+++ lib/screens/todo_overview/todo_overview.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TodoOverview extends StatelessWidget {
+  const TodoOverview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome!'),
+      ),
+    );
+  }
+}
